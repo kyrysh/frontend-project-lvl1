754ffc5e1f1c291e08025f3a9c3609d9c6cf98b2
kyrysh <victoria.tyagleeva@gmail.com>
2021-01-17 17:22:03 +0200

    Step4: Game Brain Even. Changing brain-even.js: adding game code

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,3 +1,29 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
 
-console.log('Hello');
+console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}!`);
+console.log('I will give you 3 numbers. Answer "yes" if the number is even, otherwise answer "no". If you answer is wrong - the game will be finished.');
+
+// Returns an Array of three random numbers
+const randomNumbers = [...Array(3)].map(() => Math.floor(Math.random() * 20) + 1);
+const isNumberEven = (number) => (number % 2 === 0 ? 'yes' : 'no');
+
+const brainEvenGame = () => {
+  // eslint-disable-next-line no-restricted-syntax
+  for (const number of randomNumbers) {
+    console.log(`Question: ${number}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (isNumberEven(number) === userAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${isNumberEven(number)}'. Let's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+brainEvenGame();
